9e120d9cb26cdc30654601a985f014831544b9fe
N-Ashworth <n_ashworth2102@outlook.com>
2026-06-05 16:27:06 +0000

    Fixed CORS for codespace (again)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=[
     "https://n-ashworth.github.io",
-    "https://glorious-pancake-5g59x97v5p75c476p.github.dev" #my codespace (for preview)
+    "https://glorious-pancake-5g59x97v5p75c476p-3000.app.github.dev" #my codespace (for preview)
 ])
 
 creatures = [
